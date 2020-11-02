9df5459f32404410aa54a8472f7bc330c93d11da
mathewsdt <70237239+mathewsdt@users.noreply.github.com>
2020-11-02 07:27:15 -0700

    added functions
    added my first function

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_first_function() {
+    var sentence = "I am learning" ;
+    sentence += " a lot from this course!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+
