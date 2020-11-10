edc68969c0d75d80277dea5ac7b44cc5936ba631
mathewsdt <70237239+mathewsdt@users.noreply.github.com>
2020-11-10 13:38:52 -0700

    added comments
    added Javescripts comment

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -2,5 +2,5 @@ function My_first_function() {
     var sentence = "I am learning" ;
     sentence += " a lot from this course!";
     document.getElementById("concatenate").innerHTML = sentence;
-}
+} // created a function to concatenate a var sentence along with another sentence//
 
