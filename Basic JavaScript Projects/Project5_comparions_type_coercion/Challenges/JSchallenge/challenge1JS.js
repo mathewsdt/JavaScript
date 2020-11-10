13baa45533696f8ef2fe5568213d32cf66a467d3
mathewsdt <70237239+mathewsdt@users.noreply.github.com>
2020-11-10 10:53:18 -0700

    added project 5
    added javascript project  5 and challenges

---
+++ Basic JavaScript Projects/Project5_comparions_type_coercion/Challenges/JSchallenge/challenge1JS.js
@@ -0,0 +1,5 @@
+function my_function() {
+    document.getElementById("test") .innerHTML = isNaN ('009')
+}
+
+console.log( 10 < 1);
---
+++ Basic JavaScript Projects/Project5_comparions_type_coercion/JS/main.js
@@ -0,0 +1,47 @@
+document.write(typeof "word");// using the "type of " operator to give the data type a variable//
+
+document.write(typeof 7);// using the "type of" operator to give the data type a number variable//
+
+function my_function() {
+    document.getElementById("infinity").innerHTML
+} // created a function to display infinity on click //
+
+document.write(-3E315); //used document write method to display -infinity by useing number lower than -3E310 //
+
+document.write(10 < 1); // document method to display a false boolean comparison
+
+document.write(10 > 1); // document method to display a true boolean expression
+
+console.log(3 + 3); // used to console.log method to display an addition answer in the console of developer tools //
+
+document.write("15" + 5); // Used document write method to show an exaomple of " type coercion" //
+
+document.write(4 == 4); // Used document write method to show an true comparison using "==" //
+
+document.write(10 + 3 == 15); // Used document write method to show an false comparison using "=="//
+
+a = 29;
+b = 29;
+document.write(a === b); // Used dcoument write method to show a true comparison using "===" //
+
+a = 38;
+boy = 16;
+document.write(a === boy); //used document write method to show a false comparison due to different data type and value //
+
+a = 22;
+y = 22;
+document.write(a === boy); //used document write method to show a false comparison due to different data type //
+
+a = 25;
+a = 35;
+document.write(a === b); // used document write method to show a false comparison due to different value types//
+
+document.write(5 < 10 || 10 > 5); // used (or)operator to show true comparison //
+
+document.write(5 < 4 || 4 < 3); // used (or) operator to show false comparison //
+
+function not_function() {
+    document.getElementById("not").innerHTML = ! ( 5 > 8);
+} // used "not" operator to invert boolen value //
+
+
